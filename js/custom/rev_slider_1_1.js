215602133f307e6c28eef01bce6f4eb025853ac0
valiantcreative33 <ruben@valiantcreative.net>
2021-10-13 01:30:07 -0400

    put rev_slider location in js files back to original

--- js/custom/rev_slider_1_1.js
+++ js/custom/rev_slider_1_1.js
@@ -28,7 +28,7 @@
 				}else{
 					revapi1 = tpj("#rev_slider_1_1").show().revolution({
 						sliderType:"standard",
-						jsFileLocation:"./js/custom/rev_slider_1_1.js",
+						jsFileLocation:"//melody.ancorathemes.com/wp-content/plugins/revslider/public/assets/js/",
 						sliderLayout:"auto",
 						dottedOverlay:"none",
 						delay:9000,
--- js/custom/rev_slider_2_1.js
+++ js/custom/rev_slider_2_1.js
@@ -47,7 +47,7 @@ tpj(document).ready(function() {
 	} else {
 		revapi2 = tpj("#rev_slider_2_1").show().revolution({
 			sliderType: "standard",
-			jsFileLocation: "./js/custom/rev_slider_2_1.js",
+			jsFileLocation: "//melody.ancorathemes.com/wp-content/plugins/revslider/public/assets/js/",
 			sliderLayout: "auto",
 			dottedOverlay: "none",
 			delay: 9000,
