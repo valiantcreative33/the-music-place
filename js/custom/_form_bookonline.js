52d719bee0d4f4adbec02c5787a850dc387c9a0e
valiantcreative33 <ruben@valiantcreative.net>
2022-01-25 00:05:44 -0500

    created and updated the book online form using formspree

--- js/custom/_form_bookonline.js
+++ js/custom/_form_bookonline.js
@@ -1,18 +1,17 @@
-// Contact Form Using Formspree
+// Bookonline Form Using Formspree
 
 window.addEventListener("DOMContentLoaded", function () {
   // get the form elements defined in your form HTML above
 
-  var form = document.getElementById("my-form");
-  // var button = document.getElementById("my-form-button");
-  var status = document.getElementById("status");
+  var form = document.getElementById("book_form");
+  var status = document.getElementById("book_status");
 
   // Success and Error functions for after the form is submitted
 
   function success() {
     form.reset();
     status.classList.add("success");
-    status.innerHTML = "Message sent. Thanks!";
+    status.innerHTML = "Registration sent. Thanks!";
   }
 
   function error() {
@@ -20,7 +19,7 @@ window.addEventListener("DOMContentLoaded", function () {
     status.innerHTML = "Oops! There was a problem.";
   }
 
-  // handle the form submission event
+  // handle the form registration event
 
   form.addEventListener("submit", function (ev) {
     ev.preventDefault();
